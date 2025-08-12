9edb224541c6982cd93d9555b2787745d1f23d17
Nivas-git07 <itsmenivas007@gmail.com>
2025-08-12 18:10:36 +0000

    genric function

--- tsplayground/src/interface.ts
+++ tsplayground/src/interface.ts
@@ -1,9 +1,9 @@
-interface Computer{
+interface Computer {
     name: string;
     ram: number;
     processor: string;
     storage: number;
-    
+
 }
 const Computer1: Computer = {
     name: "Asusvivobook",
@@ -16,9 +16,9 @@ console.log(Computer1);
 console.log(Computer1.name);
 console.log(Computer1.ram);
 console.log(Computer1.processor);
-console.log(Computer1.storage); 
+console.log(Computer1.storage);
 
-interface mathoperation{
+interface mathoperation {
     (a: number, b: number): number;
 }
 const add: mathoperation = (a, b) => {
@@ -34,20 +34,20 @@ interface person {
     sayhello(): void;
 }
 
-function greet(person: person){
+function greet(person: person) {
     console.log(`Hello, my name is ${person.name} and I am ${person.age} years old.`);
     person.sayhello();
 }
 
-const person1:person= {
+const person1: person = {
     name: "nivas",
     age: 25,
     sayhello: () => {
         console.log("Say hello to everyone!");
-}
+    }
 
 }
-const person2:person = {
+const person2: person = {
     name: "saravanesh",
     age: 30,
     sayhello: () => {
@@ -57,7 +57,7 @@ const person2:person = {
 greet(person1);
 greet(person2)
 
-interface moviedetails{
+interface moviedetails {
     title: string;
     director: string;
     releaseyear: number;
@@ -73,8 +73,64 @@ const movie1: moviegenre = {
     director: "loki",
     releaseyear: 2025,
     genre: "action",
-    printinfo: function() {
+    printinfo: function () {
         console.log(`Title: ${this.title}, Director: ${this.director}, Release Year: ${this.releaseyear}, Genre: ${this.genre}`);
-}
+    }
 }
 movie1.printinfo();
+
+// interface merging
+interface car {
+    brand: string;
+    start(): void;
+}
+interface car {
+    model: string;
+    stop(): void;
+}
+
+const mycar: car = {
+    brand: "BMW",
+    model: "X5",
+    start() {
+        console.log(`The ${this.brand} ${this.model} is starting.`);
+
+    },
+    stop() {
+        console.log(`The ${this.brand} ${this.model} is stopping.`);
+    }
+}
+
+mycar.start();
+mycar.stop();
+
+// generic function
+function identity<t>(arg: t): t {
+    return arg;
+}
+const result1 = identity<string>("Hello, TypeScript!");
+const result2 = identity<number>(42);
+const result3 = identity<boolean>(true);
+console.log(result1); // Output: Hello, TypeScript!
+console.log(result2); // Output: 42
+console.log(result3); // Output: true
+
+//genric interface
+function uniquedatatype<T>(item: T, defaultvalue: T): T {
+    return [item, defaultvalue];
+}
+interface dog {
+    name: string;
+    breed: string;
+}
+
+const dog = uniquedatatype<dog>({ name: "Buddy", breed: "Golden Retriever" }, { name: "Max", breed: "Labrador" });
+console.log(dog);
+
+function arrayfilter<T>(array: T[],condition:(item:T)=> boolean): T[] {
+    return array.filter(condition);
+}
+const numbers = [1, 2, 3, 4, 5];
+const evenNumbers = arrayfilter<number>(numbers, (num) => num % 2 === 0);
+console.log(evenNumbers); 
+
