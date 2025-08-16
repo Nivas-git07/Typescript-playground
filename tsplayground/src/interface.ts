b45ade89457e9ca52fcc8e701cf5c3af186cc8c7
Nivas-git07 <itsmenivas007@gmail.com>
2025-08-16 14:25:21 +0000

    type narrowing

--- tsplayground/src/interface.ts
+++ tsplayground/src/interface.ts
@@ -117,7 +117,7 @@ console.log(result3); // Output: true
 
 //genric interface
 function uniquedatatype<T>(item: T, defaultvalue: T): T {
-    return [item, defaultvalue];
+    return item || defaultvalue;
 }
 interface dog {
     name: string;
@@ -125,12 +125,85 @@ interface dog {
 }
 
 const dog = uniquedatatype<dog>({ name: "Buddy", breed: "Golden Retriever" }, { name: "Max", breed: "Labrador" });
+
 console.log(dog);
 
-function arrayfilter<T>(array: T[],condition:(item:T)=> boolean): T[] {
+function arrayfilter<T>(array: T[], condition: (item: T) => boolean): T[] {
     return array.filter(condition);
 }
 const numbers = [1, 2, 3, 4, 5];
 const evenNumbers = arrayfilter<number>(numbers, (num) => num % 2 === 0);
-console.log(evenNumbers); 
+console.log(evenNumbers);
 
+class box<T>{
+    private content: T;
+    constructor(initalcontent: T){
+        this.content = initalcontent;
+    }
+    getcontent(): T{
+        return this.content;
+    }
+    setcontent(newcontent: T): void{
+        this.content = newcontent;
+    }
+}
+const stringBox = new box<string>("Hello, World!");
+console.log(stringBox.getcontent()); // Output: Hello, World!
+stringBox.setcontent("New Content");
+console.log(stringBox.getcontent()); // Output: New Content
+
+type value = number | string | boolean;
+function processvalue(value: value): void {
+    if (typeof value === "number") {
+        console.log(`Processing number: ${value.toFixed()}`);
+    } else if (typeof value === "string") {
+        console.log(`Processing string: ${value.toUpperCase()}`);
+    } else if (typeof value === "boolean") {
+        console.log(`Processing boolean: ${value}`);
+    }
+}
+processvalue(42); // Output: Processing number: 42
+processvalue("Hello"); // Output: Processing string: HELLO
+processvalue(true); // Output: Processing boolean: true
+
+class Dog {
+    bark(): void {
+        console.log("Woof! Woof!");
+    }
+}
+class cat {
+    meow(): void {
+        console.log("Meow! Meow!");
+    }
+}
+
+function animalsound(animal: Dog | cat): void {
+    if (animal instanceof Dog) {
+        animal.bark();
+    } else if (animal instanceof cat) {
+        animal.meow();
+    }
+}
+const myDog = new Dog();
+const myCat = new cat();
+animalsound(myDog); // Output: Woof! Woof!
+animalsound(myCat); // Output: Meow! Meow!
+
+type employees = {
+    name: string;
+    age: number;
+}
+
+type manager = {
+    department: string;
+    role: string;
+}
+type employeeDetails = employees & manager;
+const manager1: employeeDetails = {
+    name: "nivas",
+    age: 18,
+    department: "IT",
+    role: "Software Engineer"
+
+}
+console.log(manager1);
